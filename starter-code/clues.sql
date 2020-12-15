3dfc3ac4e5e163503e06e46e74dd8297a76d9a11
Henry Lee <henrylee206@gmail.com>
2020-12-15 15:12:04 -0800

    25% done

--- starter-code/clues.sql
+++ starter-code/clues.sql
@@ -1,19 +1,33 @@
 -- Clue #1: We recently got word that someone fitting Carmen Sandiego's description has been
 -- traveling through Southern Europe. She's most likely traveling someplace where she won't be noticed,
 -- so find the least populated country in Southern Europe, and we'll start looking for her there.
+SELECT * from country where region = 'Southern Europe' order by population asc limit 1; --Vatican
 
 
 
 -- Clue #2: Now that we're here, we have insight that Carmen was seen attending language classes in
 -- this country's officially recognized language. Check our databases and find out what language is
 -- spoken in this country, so we can call in a translator to work with you.
+/*
+ code |             name              | continent |     region      | surfacearea | indepyear | population | lifeexpectancy | gnp  | gnpold |            localname            |      governmentform      |     headofstate     | capital | code2 
+------+-------------------------------+-----------+-----------------+-------------+-----------+------------+----------------+------+--------+---------------------------------+--------------------------+---------------------+---------+-------
+ VAT  | Holy See (Vatican City State) | Europe    | Southern Europe |         0.4 |      1929 |       1000 |                | 9.00 |        | Santa Sede/Cittï¿½ del Vaticano | Independent Church State | Johannes Paavali II |    3538 | VA
+(1 row)
+*/
+carmen=# SELECT language from countrylanguage where countrycode = 'VAT' and isofficial = 't';
+--  language 
+-- ----------
 
-
+--  Italian
+-- (1 row)
 
 -- Clue #3: We have new news on the classes Carmen attended – our gumshoes tell us she's moved on
 -- to a different country, a country where people speak only the language she was learning. Find out which
 --  nearby country speaks nothing but that language.
 
+SELECT *  from country INNER JOIN countrylanguage ON country.code = countrylanguage.countrycode where region = 'Southern Europe' order by name;
+
+--San Marino
 
 
 -- Clue #4: We're booking the first flight out – maybe we've actually got a chance to catch her this time.
